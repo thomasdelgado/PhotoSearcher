ad65d8e4aef14fa67a567e905d02bd26b8030739
Thomas <thomas.ddelgado@gmail.com>
2021-06-08 15:33:26 -0300

    commit file project

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Thomas Delgado on 08/06/21.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
